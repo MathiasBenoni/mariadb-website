24240129af2b8cde6fec017a42158724267d0580
Mathias Moen <mathiasbbmoen@gmail.com>
2026-01-28 17:18:51 +0100

    Got JS connected to the flask

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Index Page'
+    return render_template("index.html")
 
-@app.route('/nisse')
-def hello():
-    return 'Hei, hva gjør du her???'
+if __name__ == "__main__":
+    app.run(debug=True)
