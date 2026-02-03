859f4866fa1db2620ffef6ca8101a750401f2634
Mathias Moen <mathiasbbmoen@gmail.com>
2026-02-03 15:02:26 +0100

    Relized I do not need the write function, I can just return a sting

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import *
-from mariabd_python import *
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    write("x")
-    return render_template("index.html")
+    message = "Hello from python!!!"
+    return render_template("index.html", message = message)
 
 @app.route('/submit', methods=['POST'])
 def handle_data():
     if request.method == 'POST':
-        
         first_name = request.form.get('fname')
         last_name = request.form.get('lname')
 
         print(f"Received: {first_name}")
         print(f"Received: {last_name}")
-        # Return a response or redirect to another page
+
+        # Return a response using write()
         return f'Data received! Your name is: {first_name} {last_name}'
     
     return redirect(url_for('index'))
-
-
-def write(x):
-    return f'{x}'
 
 if __name__ == "__main__":
     app.run(debug=True)
