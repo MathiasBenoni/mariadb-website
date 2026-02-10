31f31faf5ebf0bf2513addd555b219f39b685875
Mathias Moen <mathiasbbmoen@gmail.com>
2026-02-10 13:46:02 +0100

    Deleted name form

--- app.py
+++ app.py
@@ -25,26 +25,11 @@ def index():
                 if adjectives_list[i] > bigger_number:
                     bigger_number = adjectives_list[i]
                     max_index = i 
-        
         the_adjective = adjectives_list[max_index - 1].capitalize()
     else:
         the_adjective = "Nothing here yet"
 
     return render_template("index.html", html_adjective_list=adjectives_list, the_adjective_html=the_adjective)
-
-@app.route('/submit', methods=['POST'])
-def handle_data():
-    if request.method == 'POST':
-        first_name = request.form.get('fname')
-        last_name = request.form.get('lname')
-
-        print(f"Received: {first_name}")
-        print(f"Received: {last_name}")
-
-        # Return a response using write()
-        return f'Data received! Your name is: {first_name} {last_name}'
-    
-    return redirect(url_for('index'))
 
 @app.route("/", methods=["POST"])
 def add():
