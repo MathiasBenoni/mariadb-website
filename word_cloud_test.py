57c4f7697d0714c3de36fc464c49bc1d3120a28a
Mathias Moen <mathiasbbmoen@gmail.com>
2026-02-11 11:58:48 +0100

    Added dark and light mode wordclouds, and a way to display them on the website

--- word_cloud_test.py
+++ word_cloud_test.py
@@ -6,8 +6,8 @@ from PIL import Image
 text = open("text.txt", 'r').read()
 
 # Load and resize FIRST
-mask_image = Image.open("images/joker.png")
-mask_image = mask_image.resize((500, 750))  # Resize before converting to array
+mask_image = Image.open("images/python.jpeg")
+mask_image = mask_image.resize((500, 500))  # Resize before converting to array
 
 # Convert to numpy array
 python_mask = np.array(mask_image)
@@ -15,17 +15,25 @@ python_mask = np.array(mask_image)
 # Create color generator from the numpy array
 colourmap = ImageColorGenerator(python_mask)
 
-wc = WordCloud(stopwords=STOPWORDS,
-               mask=python_mask,
-               background_color="white",
+wc = WordCloud(mask=python_mask,
+               background_color="black",
                max_words=2000,
-               min_font_size=3).generate(text)
+               min_font_size=2).generate(text)
 
 # Recolor using the image colors
 wc.recolor(color_func = colourmap)
 
 # Save the wordcloud
-wc.to_file("images/output_wordcloud.png")
+wc.to_file("static/images/output_wordcloud_dark.png")
 
+
+wc = WordCloud(mask=python_mask,
+               background_color="white",
+               max_words=2000,
+               min_font_size=2).generate(text)
+
+wc.recolor(color_func = colourmap)
+
+wc.to_file("static/images/output_wordcloud_light.png")
 
 plt.imshow(wc, interpolation='bilinear')
