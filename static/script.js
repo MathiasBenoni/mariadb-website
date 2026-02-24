32735e007f0a00ab7634be39d1046ca82eb44963
Mathias Moen <mathiasbbmoen@gmail.com>
2026-02-24 13:15:34 +0100

    The wordcloud now updates with the theme

--- static/script.js
+++ static/script.js
@@ -2,13 +2,21 @@ console.log("Hello from JS");
 
 const lightTheme = "/static/lightmode.css";
 const darkTheme = "/static/darkmode.css";
+const darkPic = "/static/images/cloud_dark.png";
+const lightPic = "/static/images/cloud_light.png";
+
+const image = document.getElementById("wordcloud");
+
+console.log(image);
 
 function toggleTheme() {
   const themeLink = document.getElementById("theme-style");
 
   if (themeLink.getAttribute("href").includes("lightmode.css")) {
     themeLink.setAttribute("href", darkTheme);
+    image.setAttribute("src", darkPic);
   } else {
     themeLink.setAttribute("href", lightTheme);
+    image.setAttribute("src", lightPic);
   }
 }
